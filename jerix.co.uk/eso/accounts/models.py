13d0d25a4d2a11470f4dbb3395402c0c7077c9c8
Thomas Purchas <tpurchas@gmail.com>
2012-07-24 02:24:45 +0100

    Improved accounts app.
    We now have a new AuthoredObject model ready to go. This can be subclassed to
    give any object an author.
    
    Refactored the profile stuff a little and added the year field to the student
    profile.

--- jerix.co.uk/eso/accounts/models.py
+++ jerix.co.uk/eso/accounts/models.py
@@ -1,20 +1,26 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, SiteProfileNotAvailable
 from django.db.models.signals import post_save
 
 # Create your models here.
 class StudentProfile(models.Model):
     """(StudentProfile description)"""
-    
+
+    year = models.CharField(blank=False, max_length=30)
+    tutor = models.ForeignKey(User)
 
     class Admin:
         list_display = ('',)
         search_fields = ('',)
 
     def __unicode__(self):
-        return u"StudentProfile"
+        try:
+            name = self.userprofile.user.username
+        except:
+            name = u"StudentProfile"
+        return name
 
-        
+
 class LecturerProfile(models.Model):
     """(LectureProfile description)"""
 
@@ -27,11 +33,23 @@ class LecturerProfile(models.Model):
 
 class UserProfile(models.Model):
     """Represents a user"""
-    
+
     user = models.OneToOneField(User)
-    
-    lecturer_profile = models.OneToOneField(LecturerProfile, null=True, blank=True, related_name='user')
-    student_profile = models.OneToOneField(StudentProfile, null=True, blank=True, related_name='user')
+
+    title = models.CharField(blank=True, max_length=30)
+
+    lecturer_profile = models.OneToOneField(LecturerProfile, null=True, blank=True)
+    student_profile = models.OneToOneField(StudentProfile, null=True, blank=True)
+
+    def full_name(self):
+        """docstring for full_name"""
+
+        return u'%s %s' % (self.user.first_name, self.user.last_name)
+
+    def full_title(self):
+        """docstring for full_title"""
+
+        return u'%s %s' % (self.title, self.full_name())
 
     class Admin:
         list_display = ('',)
@@ -39,6 +57,31 @@ class UserProfile(models.Model):
 
     def __unicode__(self):
         return unicode(self.user.username)
+
+class AuthoredObject(models.Model):
+    """(AuthoredObject description)"""
+
+    author = models.ForeignKey(User)
+
+    def author_full_name(self):
+        """docstring for full_name"""
+
+        return u'%s %s' % (self.author.first_name, self.author.last_name)
+
+    def author_full_title(self):
+        """docstring for author_full_title"""
+
+        return self.author.get_profile().full_title()
+
+    class Meta:
+        abstract = True
+
+    class Admin:
+        list_display = ('',)
+        search_fields = ('',)
+
+    def __unicode__(self):
+        return u"AuthoredObject"
 
 
 def create_user_profile(sender, instance, created, **kwargs):
