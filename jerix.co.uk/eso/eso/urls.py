0a8d4d1680b762a70854a64f6e1589fd6c642d72
Thomas Purchas <tpurchas@gmail.com>
2012-08-08 04:29:34 +0100

    Add a MEDIA_URL and debug serve page.
    Setup django to serve media files automagically in debug mode using the dirty `serve()` view.

--- jerix.co.uk/eso/eso/urls.py
+++ jerix.co.uk/eso/eso/urls.py
@@ -18,3 +18,10 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+   )
