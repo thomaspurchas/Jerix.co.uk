62b9ce9db4f5fc956174989cc77972dce5045102
Thomas Purchas <tpurchas@gmail.com>
2012-08-08 03:32:59 +0100

    Add quite a sophisticated admin interface for Document's
    Added an admin interface which plays nice with the way documents and blobs are meant to be created. The admin site has been modified to hide the use of blobs in the document page. Instead there is just a file upload widget.
    
    When a new document is created or a new file is uploaded to a document it is passed through the standard de-duplication layer and blob are created and destroyed as necessary.

---
+++ jerix.co.uk/eso/files/admin.py
@@ -0,0 +1,54 @@
+from django.contrib import admin
+from django import forms
+
+from django.core.files.uploadedfile import UploadedFile
+
+from files.models import Document, DerivedDocument, ParentBlob, DerivedBlob
+
+class DocumentForm(forms.ModelForm):
+    class Meta:
+        model = Document
+        #exclude = ('_blob')
+
+    file = forms.FileField(required=True)
+
+    def __init__(self, *args, **kwargs):
+        new = False if kwargs.get('instance') else True
+        if not kwargs.get('initial') and not new:
+            kwargs['initial'] = {'file': kwargs['instance']._blob.file}
+        super(DocumentForm, self).__init__(*args, **kwargs)
+        self.fields['title'].required = False
+        self.fields['file_name'].required = False
+
+class ParentBlobAdmin(admin.ModelAdmin):
+    readonly_fields = ('md5_sum',)
+
+class DerivedBlobAdmin(admin.ModelAdmin):
+    readonly_fields = ('md5_sum',)
+
+class DocumentAdmin(admin.ModelAdmin):
+    form = DocumentForm
+    exclude = ['_blob']
+
+    def add_view(self, *args, **kargs):
+        self.exclude.append('file_name')
+        return super(DocumentAdmin, self).add_view(*args, **kargs)
+
+    def save_model(self, request, obj, form, change):
+        if isinstance(form.cleaned_data['file'], UploadedFile):
+            obj.file = form.cleaned_data['file']
+        obj.save()
+        if obj.file_name.strip() == '':
+            obj.file_name = ':'.join(obj.file.name.split(':')[1:])
+
+        if obj.title.strip() == '':
+            obj.title = obj.file_name
+        obj.save()
+
+class DerivedDocumentAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(ParentBlob, ParentBlobAdmin)
+admin.site.register(DerivedBlob, DerivedBlobAdmin)
+admin.site.register(Document, DocumentAdmin)
+admin.site.register(DerivedDocument, DerivedDocumentAdmin)
