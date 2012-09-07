776ddef30a44839433f13ac4c447476ca5d0c67b
Thomas Purchas <tpurchas@gmail.com>
2012-09-07 02:21:41 +0100

    Fix typo

--- jerix.co.uk/eso/eso/settings.py
+++ jerix.co.uk/eso/eso/settings.py
@@ -78,7 +78,7 @@ MEDIA_ROOT = pathjoin(SITE_ROOT, 'media/')
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = '//media.jerix.co.uk'
+MEDIA_URL = '//media.jerix.co.uk/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
