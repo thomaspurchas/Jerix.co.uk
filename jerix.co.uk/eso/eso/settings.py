a6c135ce6e5d58c2ed646e6383247f02e73b46d0
Thomas Purchas <tpurchas@gmail.com>
2012-09-11 18:47:02 +0100

    Get DEBUG mode from environment variables

--- jerix.co.uk/eso/eso/settings.py
+++ jerix.co.uk/eso/eso/settings.py
@@ -3,8 +3,8 @@ from os.path import join as pathjoin
 import djcelery
 # Django settings for eso project.
 
-DEBUG = True
-TEMPLATE_DEBUG = DEBUG
+DEBUG = os.environ.get('DEBUG', "true").lower() == "true"
+TEMPLATE_DEBUG = os.environ.get('TEMPLATE_DEBUG', "true").lower() == "true"
 
 ADMINS = (
     ('Thomas Purchas', 'thomas@jerix.co.uk'),
@@ -78,7 +78,7 @@ MEDIA_ROOT = pathjoin(SITE_ROOT, 'media/')
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = '//media.jerix.co.uk/'
+MEDIA_URL = 'media' if DEBUG else '//media.jerix.co.uk/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
@@ -88,7 +88,7 @@ STATIC_ROOT = pathjoin(SITE_ROOT, 'static/')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '//static.jerix.co.uk/'
+STATIC_URL = '/static/' if DEBUG else '//static.jerix.co.uk/'
 
 STATIC_DOC_ROOT = pathjoin(SITE_ROOT, 'static/')
 
