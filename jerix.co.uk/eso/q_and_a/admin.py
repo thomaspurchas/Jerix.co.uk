bff93e1b12f9d0040a1201f9a1a75b90b9283ca3
Thomas Purchas <tpurchas@gmail.com>
2012-09-14 02:57:46 +0100

    Put answers inline with questions

--- jerix.co.uk/eso/q_and_a/admin.py
+++ jerix.co.uk/eso/q_and_a/admin.py
@@ -2,9 +2,18 @@ from django.contrib import admin
 
 from q_and_a.models import Question, Answer
 
+class AnswerInline(admin.TabularInline):
+    """docstring for SubPostInline"""
+    model = Answer
+    fk_name = 'question'
+    extra = 2
+    classes = ['collapse', 'collapsed']
+    exclude = ['reputation_owner', 'reputation']
+
 class QuestionAdmin(admin.ModelAdmin):
     exclude = ['reputation_owner', 'reputation']
     readonly_fields = ['current_vote']
+    inlines = [AnswerInline]
 
 class AnswerAdmin(admin.ModelAdmin):
     exclude = ['reputation_owner', 'reputation']
