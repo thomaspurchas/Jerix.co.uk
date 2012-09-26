c4f35369151b21ed087425ed92c60abc1995abb7
Thomas Purchas <tpurchas@gmail.com>
2012-09-26 16:20:57 +0100

    Add Pygments, but don't use it.
    Adding Pygments, but I am not currently using it because it's guessing is below par.

--- jerix.co.uk/eso/q_and_a/templatetags/markdown_tags.py
+++ jerix.co.uk/eso/q_and_a/templatetags/markdown_tags.py
@@ -1,3 +1,5 @@
+import re
+
 from django.template import Library
 from django.utils.encoding import force_unicode
 from django.utils.functional import allow_lazy
@@ -6,6 +8,10 @@ from django.utils.safestring import mark_safe
 
 import markdown
 import bleach
+import pygments
+from pygments import lexers
+from pygments import formatters
+
 
 safe_tags = [
             'b', 'blockquote', 'code', 'del', 'dd', 'dl', 'dt', 'em', 'h1',
@@ -23,9 +29,37 @@ register = Library()
 @register.filter('markdown')
 @stringfilter
 def markdown_tag(s):
-    print s
     s = markdown.markdown(s, safe_mode=False)
-    print s
     s = bleach.clean(s, safe_tags, safe_attr, strip=True)
-    print s
     return mark_safe(s)
+
+regex = re.compile(r'<code(.*?)>(.*?)</code>', re.DOTALL)
+
+@register.filter(name='pygmentize')
+def pygmentize(value):
+    last_end = 0
+    to_return = ''
+    found = 0
+    for match_obj in regex.finditer(value):
+        code_class = match_obj.group(1)
+        code_string = match_obj.group(2)
+        if code_class.find('class') > -1:
+            language = re.split(r'"|\'', code_class)[1]
+            lexer = lexers.get_lexer_by_name(language)
+        else:
+            try:
+                lexer = lexers.guess_lexer(str(code_string))
+            except ValueError:
+                lexer = lexers.PythonLexer()
+        print code_string
+        print lexer
+        print lexer.analyse_text(str(code_string))
+        print lexers.PythonLexer().analyse_text(str(code_string))
+        pygmented_string = pygments.highlight(code_string, lexer, formatters.HtmlFormatter(nowrap=True))
+        to_return = (to_return + value[last_end:match_obj.start(0)] +
+                    '<code class="default prettyprint">' + code_string + #pygmented_string +
+                    '</code>')
+        last_end = match_obj.end(2)
+        found = found + 1
+    to_return = to_return + value[last_end:]
+    return mark_safe(to_return)
