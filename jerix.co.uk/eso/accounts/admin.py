255185b5006056c8c35951b3a8be08774fce021a
Thomas Purchas <tpurchas@gmail.com>
2012-10-15 18:14:49 +0100

    Use admin inlines in LectureProfile and StudentProfile

--- jerix.co.uk/eso/accounts/admin.py
+++ jerix.co.uk/eso/accounts/admin.py
@@ -1,9 +1,21 @@
 from django.contrib import admin
 from accounts.models import UserProfile, LecturerProfile, StudentProfile
 
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+
 class LecturerProfileAdmin(admin.ModelAdmin):
     filter_horizontal = ('modules',)
+    inlines = [
+        UserProfileInline,
+    ]
+
+class StudentProfileAdmin(admin.ModelAdmin):
+    filter_horizontal = ('modules',)
+    inlines = [
+        UserProfileInline,
+    ]
 
 admin.site.register(UserProfile)
 admin.site.register(LecturerProfile, LecturerProfileAdmin)
-admin.site.register(StudentProfile)
+admin.site.register(StudentProfile, StudentProfileAdmin)
