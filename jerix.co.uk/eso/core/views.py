a77fc8bdf2a680cf2d960949cc9d03666c81e18a
Thomas Purchas <tpurchas@gmail.com>
2012-10-25 01:10:43 +0100

    Improve home page column handling and add "Recent Questions"
    Improved the way multiple columns are handled. The number of items in each column is dynamically determined based on the number of items that we are trying to display.
    
    From the dictionary of all the "blobs" that need to be displayed the view will use an iterator to place half of the blobs in one column and the rest in the other. Using the iterator allows the blobs to be laid out from left-to-right, then continuing onto the row below rather than stacking the first of the items in the first column and the second half in the second column.

--- jerix.co.uk/eso/core/views.py
+++ jerix.co.uk/eso/core/views.py
@@ -3,13 +3,14 @@ from django.template import RequestContext
 from django.shortcuts import render_to_response, redirect, get_object_or_404
 
 from accounts.models import UserProfile
+from q_and_a.models import Question
 
 # Create your views here.
 
 def home(request):
     user = request.user
     modules = []
-    context = {}
+    items = {}
     if request.user.is_authenticated():
         profile = user.get_profile()
         if profile.is_student():
@@ -19,10 +20,34 @@ def home(request):
             lecturers = UserProfile.objects.filter(
                             lecturer_profile__modules__students=student_profile)
 
-            context.update({
+            items.update({
                 'modules': modules,
                 'lecturers': lecturers
             })
+
+    questions = Question.objects.all().order_by('-asked')[:10]
+
+    items.update({
+        'questions': questions,
+    })
+
+    col_1 = []
+    col_2 = []
+
+    iterator = items.iteritems()
+
+    while True:
+        try:
+            col_1.append(iterator.next())
+            col_2.append(iterator.next())
+        except StopIteration:
+            break
+    print col_1
+    print col_2
+    context = {
+        'col_1': col_1,
+        'col_2': col_2
+    }
 
     return render_to_response(
         'core/home.html',
