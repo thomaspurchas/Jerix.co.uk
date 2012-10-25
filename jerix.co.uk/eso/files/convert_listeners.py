6e92ccb17b4b0dbedca0707cef31d3074f40b53f
Thomas Purchas <tpurchas@gmail.com>
2012-10-25 14:43:39 +0100

    Remove print statements and use log.info

--- jerix.co.uk/eso/files/convert_listeners.py
+++ jerix.co.uk/eso/files/convert_listeners.py
@@ -1,12 +1,15 @@
+import logging
+
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from files.models import ParentBlob
 
+log = logging.getLogger(__name__)
+
 @receiver(post_save, sender=ParentBlob)
 def convert_to_pdf(sender, instance, created, raw, **kargs):
     from files.tasks import create_pdf
-    print 'Covert to PDF'
     if not raw:
-        print 'Firing create_pdf task with %s:%s' % (instance, instance.pk)
+        log.info('Firing create_pdf task with %s:%s' % (instance, instance.pk))
         create_pdf.delay(instance.pk)
