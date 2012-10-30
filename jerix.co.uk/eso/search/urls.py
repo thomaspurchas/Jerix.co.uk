4f599ee5ed471d84583d8de351f3f4e60d519cef
Thomas Purchas <tpurchas@gmail.com>
2012-10-30 20:56:44 +0000

    Change number of result per page.

--- jerix.co.uk/eso/search/urls.py
+++ jerix.co.uk/eso/search/urls.py
@@ -3,5 +3,5 @@ from haystack.views import SearchView
 
 
 urlpatterns = patterns('haystack.views',
-    url(r'^$', SearchView(), name='haystack_search'),
+    url(r'^$', SearchView(results_per_page=15), name='haystack_search'),
 )
