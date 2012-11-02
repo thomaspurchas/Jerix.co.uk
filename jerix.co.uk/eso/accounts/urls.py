d9db8ab03a40f8777a63537f79362558a3cdda70
Thomas Purchas <tpurchas@gmail.com>
2012-11-02 02:21:10 +0000

    Change login page url name from user-login to login

--- jerix.co.uk/eso/accounts/urls.py
+++ jerix.co.uk/eso/accounts/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('accounts.views',
-    url(r'^login/', 'login_user', name='user-login'),
+    url(r'^login/', 'login_user', name='login'),
 
 )
 
