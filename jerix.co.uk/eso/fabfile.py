9d837b76e4a15398c237f3d5c7a9c8a4b3e7b984
Thomas Purchas <tpurchas@gmail.com>
2013-01-21 12:47:55 +0000

    Improve fabfile.
    Add index updates and db migrations

--- jerix.co.uk/eso/fabfile.py
+++ jerix.co.uk/eso/fabfile.py
@@ -8,19 +8,34 @@ def set_aws():
 def push():
     local('git push')
 
-def stage():
+def stage_deploy():
     local('git push heroku-staging -f')
     local('STAGING=TRUE ./manage.py compress')
     local('STAGING=TRUE DEBUG=false ./manage.py collectstatic --noinput')
     local('heroku restart --app jerix-staging')
     local('rm -rf ../static')
     
-def deploy():
+def stage_migrate():
     stage()
+    local('heroku run jerix.co.uk/eso/manage.py syncdb --app jerix-staging')
+    local('heroku run jerix.co.uk/eso/manage.py migrate --app jerix-staging')
+ 
+def stage_update_index():
+    local('heroku run jerix.co.uk/eso/manage.py update_index --remove --app jerix-staging')    
+   
+def deploy(stage=True):
+    if stage: stage_deploy()
     local('git push heroku -f')
     local('DEBUG=false ./manage.py compress')
     local('DEBUG=false ./manage.py collectstatic --noinput')
     local('heroku restart --app jerix')
     local('rm -rf ../static')
-
     
+def migrate():
+    stage_migrate()
+    deploy(False)
+    local('heroku run jerix.co.uk/eso/manage.py syncdb --app jerix')
+    local('heroku run jerix.co.uk/eso/manage.py migrate --app jerix')
+
+def update_index():
+    local('heroku run jerix.co.uk/eso/manage.py update_index --remove --app jerix')
