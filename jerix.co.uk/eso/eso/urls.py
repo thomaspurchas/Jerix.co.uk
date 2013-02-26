35dcc67869228ca7880425eeaa474fe9956ae8d8
Thomas Purchas <tpurchas@gmail.com>
2013-02-26 17:25:36 +0000

    Remove normal django registration stuff

--- jerix.co.uk/eso/eso/urls.py
+++ jerix.co.uk/eso/eso/urls.py
@@ -7,10 +7,9 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', 'core.views.home', name='home'),
-    
+
     url(r'^api/', include('eso.api')),
-    
-    url(r'^account/', include('accounts.urls')),
+
     (r'^accounts/', include('allauth.urls')),
 
     url(r'^modules/(?P<module_id>\d+)/(?P<slug>.+)/?$', 'modules.views.module_posts', name="module-posts"),
