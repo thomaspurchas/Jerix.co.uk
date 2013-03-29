a355d3af6be46b4e19433a39d35e564319a31a77
Thomas Purchas <tpurchas@gmail.com>
2013-03-29 01:35:47 +0000

    Fix caching for module posts.
    This is a huge work around for a bug in django's ORM that causes problems when pickling certain querysets. An issue has been filed with django (#20157).

--- jerix.co.uk/eso/modules/views.py
+++ jerix.co.uk/eso/modules/views.py
@@ -7,7 +7,7 @@ from django.db.models import Q
 from django.template.defaultfilters import slugify
 from django.core.cache import cache
 
-from modules.models import Module
+from modules.models import Module, ParentPost
 from q_and_a.models import Question
 
 logger = logging.getLogger(__name__)
@@ -24,22 +24,23 @@ def module_posts(request, module_id, slug=None):
 
         if slug != slugify(module.title):
             return redirect('module-posts',
-                module_id=module_id, slug=slugify(module.title),
-                permanent=False)
+                            module_id=module_id, slug=slugify(module.title),
+                            permanent=False)
         posts = cache.get('module_posts_%s_posts' % module_id)
         if not posts:
-            posts = module.posts.filter(
+            posts = ParentPost.objects.filter(module=module)
+            posts = posts.filter(
                 Q(historical_period__start_date__lte=datetime.date.today()) |
                 Q(historical_period=None)
             )
             posts = posts.exclude(
-                        historical_period__end_date__lte=datetime.date.today())
-
-            # Prefetch materials, and documents
-            rel = 'materials__document___blob__derived_documents___blob'
-            posts = posts.prefetch_related(rel)
+                historical_period__end_date__lte=datetime.date.today())
 
             cache.set('module_posts_%s_posts' % module_id, posts, 60 * 0.5)
+
+        # Prefetch materials, and documents
+        rel = 'materials__document___blob__derived_documents___blob'
+        posts = posts.prefetch_related(rel)
 
         # Get related questions by looking up the modules `primary_tag`
         questions = cache.get('modules_posts_%s_questions' % module_id)
@@ -51,10 +52,10 @@ def module_posts(request, module_id, slug=None):
     except Module.DoesNotExist:
         raise Http404
     return render(request,
-        'modules/posts.html',
-        {
-            'module': module,
-            'posts': posts,
-            'questions': questions,
-        }
-    )
+                  'modules/posts.html',
+                  {
+                      'module': module,
+                      'posts': posts,
+                      'questions': questions,
+                  }
+                  )
