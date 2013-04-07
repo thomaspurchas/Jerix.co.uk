2ad5d61314f9b416a1e257160e583959af78d98c
Thomas Purchas <tpurchas@gmail.com>
2013-04-07 18:59:14 +0100

    Add a content field which just stores extracted content of the material for creating snippets

--- jerix.co.uk/eso/modules/search_indexes.py
+++ jerix.co.uk/eso/modules/search_indexes.py
@@ -1,4 +1,7 @@
 import logging
+import ast
+
+import bleach
 
 from django.template import loader, Context
 from django.db.models import signals
@@ -14,32 +17,34 @@ log = logging.getLogger(__name__)
 
 def get_content(doc, backend):
     if doc.extracted_content == None:
-        file_obj = doc.file
-        file_obj.seek(0)
         try:
-            content = backend.extract_file_contents(file_obj)
-            # Note: This may return None if it can't connect to the search
-            # server.
-            doc.extracted_content = content
-            if content == None:
-                raise SolrError('Unable to connect to server!')
-            doc.save()
-        except (SolrError, TypeError) as e:
-            content = ""
-            if not doc.extraction_error:
-                msg = ('Extracting content from: %s resulted in the ' +
-                         'following pysolr error:\n%s')
-                log.error(msg % (doc._blob, e))
-                doc.extraction_error = True
+            file_obj = doc.file
+            file_obj.seek(0)
+            try:
+                content = backend.extract_file_contents(file_obj)
+                # Note: This may return None if it can't connect to the search
+                # server.
+                doc.extracted_content = content
+                if content == None:
+                    raise SolrError('Unable to connect to server!')
                 doc.save()
-        else:
-            if doc.extraction_error == True:
-                doc.extraction_error = False
-                doc.save()
+            except (SolrError, TypeError) as e:
+                content = {u'contents': u''}
+                if not doc.extraction_error:
+                    msg = ('Extracting content from: %s resulted in the ' +
+                             'following pysolr error:\n%s')
+                    log.error(msg % (doc._blob, e))
+                    doc.extraction_error = True
+                    doc.save()
+            else:
+                if doc.extraction_error == True:
+                    doc.extraction_error = False
+                    doc.save()
+        except IOError:
+            content = {u'contents': u''}
     else:
-        content = doc.extracted_content
-
-    return content
+        content = ast.literal_eval(doc.extracted_content)
+    return bleach.clean(content['contents'], strip=True)
 
 class PostIndex(CelerySearchIndex):
     text = indexes.CharField(document=True, use_template=True)
@@ -61,8 +66,22 @@ class SubPostIndex(PostIndex, indexes.Indexable):
 
 class MaterialIndex(CelerySearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
+    content = indexes.CharField(indexed=False)
     author = indexes.CharField(model_attr='author__get_full_name', faceted=True)
     tags = indexes.MultiValueField(faceted=True)
+
+    def _get_longest_content(self, obj):
+        content = get_content(obj.document, self._get_backend(None))
+        max_len = len(content)
+
+        for doc in obj.document._blob.derived_documents.all():
+            temp_content = get_content(doc, self._get_backend(None))
+            length = len(temp_content)
+            if length > max_len:
+                content = temp_content
+                max_len = length
+
+        return content
 
     def prepare_tags(self, obj):
         if hasattr(obj.post, 'parentpost'):
@@ -77,17 +96,11 @@ class MaterialIndex(CelerySearchIndex, indexes.Indexable):
 
         return tags
 
+    def prepare_content(self, obj):
+        return self._get_longest_content(obj)
+
     def prepare_text(self, obj):
-        content = get_content(obj.document, self._get_backend(None))
-        max_len = len(content)
-
-        for doc in obj.document._blob.derived_documents.all():
-            temp_content = get_content(doc, self._get_backend(None))
-            length = len(temp_content)
-            if length > max_len:
-                content = temp_content
-                max_len = length
-
+        content = self._get_longest_content(obj)
         t = loader.select_template(('search/indexes/modules/material_text.txt',))
         return t.render(Context({'object': obj, 'extracted': content}))
 
